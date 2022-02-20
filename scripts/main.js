d33f20abb7bf8ddc019b3dc27643ce877e956169
Lucas Oliveira <87589466+LuskarDev@users.noreply.github.com>
2022-02-20 19:20:50 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,45 +19,76 @@ const Ls = document.querySelector('.L-S')
 
 const user = document.querySelector('.a-profile img')
 
+
 user.onclick = () => {
-        Ls.classList.toggle('ShowL')
+        Ls.classList.toggle('ShowLS');
 }
 
 
-/** input animation */
+        /** input animation */
 
-const Isearch = document.getElementById("Isearch");
+        const Inputsearch = document.querySelector('.search')
 
-const Inputsearch = document.querySelector('.search');
+        const Isearch = document.querySelector('.Isearch');
+
+        Isearch.onclick = () => {
+                Inputsearch.classList.toggle('showSearch');
+                Isearch.classList.toggle('moveI')
+          }
 
 
-Isearch.onclick = () => {
-        Inputsearch.classList.toggle('showSearch');
-        Isearch.classList.toggle('moveI');
-}
 
 const onload = document.querySelector('.on-load')
 
 window.addEventListener('load', () => {
         setInterval(() => {
                 onload.style.display = "none";
-        },200)
+        }, 150)
 })
+
+
 
 /** switcher theme */
 
+function switcherTheme(){
 
-const chk = document.getElementById('chk')
+        const circle = document.querySelector(".circle")
 
-const circle = document.querySelector(".circle")
+        const headrow = document.getElementById("headrow")
 
-const headrow = document.getElementById("headrow")
+        const header = document.querySelector('.header')
 
-chk.onclick = () => {
-        
+        const Alight = document.querySelectorAll('.Alight')
+
+        const logoP = document.querySelector('.logoP')
+
+        const title = document.querySelectorAll('.title')
+
+        Inputsearch.classList.toggle('darkInpt')
+
         circle.classList.toggle('ative');
+        logoP.classList.toggle('darkP');
         headrow.classList.toggle('light');
+        Isearch.classList.toggle('darki')
+        header.classList.toggle('lightH');
+        for (const list of Alight) {
+                list.classList.toggle('dark')
+        }
+        for (const titles of title) {
+                titles.classList.toggle('darkT')
+        }
+
 }
+
+
+
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('.menu-mobile ul li a')
+
+for (const link of links){
+    link.addEventListener('click', () => menu.classList.remove('showmenu-m'))
+}
+
 
 
 /** Menu mobile */
@@ -70,5 +101,6 @@ const activeMenu = document.getElementById('MenuActive');
 
 activeMenu.onclick = () => {
         menu.classList.toggle('showmenu-m');
-        activeMenu.classList.toggle('sembg')
 }
+
+
